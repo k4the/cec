ab5a1c0ab5e60282d60d6ca3ad3b0c9bfac80d35
k4the <k4thesmith@gmail.com>
2018-12-11 07:44:02 +0000

    added address first message

--- src/register/data.js
+++ src/register/data.js
@@ -16,6 +16,9 @@ const subTitles = {
   tellUs: {
       title: 'Tell us your details',
       message: 'By signing up to the Cheap Energy Club service you agree that we may regularly contact you by email about energy savings, including letting you know when you can save money, telling you about energy savings news, letting you know about energy deals and when your energy tariff is ending.'
+  },
+  addressFirst: {
+      title: 'Please tell us your address first'
   }
 };
 
